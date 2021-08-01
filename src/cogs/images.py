ed9950a4703d8367403816373cdcade76978dcc8
Nicholas Miklaucic <nicholas.miklaucic@gmail.com>
2021-07-31 21:29:10 -0400

    Fixed image search

--- src/cogs/images.py
+++ src/cogs/images.py
@@ -5,6 +5,8 @@
 from discord.ext import commands
 import discord
 import requests
+from bs4 import BeautifulSoup
+
 
 class ImageCommands(commands.Cog):
     """Images search. (Not Google because they killed API access...)"""
@@ -12,25 +14,18 @@ class ImageCommands(commands.Cog):
         self.client = client
 
     def search(self, q):
-        """Gets an image URL for the given query and number of images."""
+        """Get an image URL for the given query and number of images."""
+        url = "https://www.google.com/search"
         params = {
-            'count': 1,
             'q': q,
-            't': 'images',
-            'safesearch': 1,
-            'locale': 'en_US',
-            'uiv': 4
+            'tbm': 'isch'
         }
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
-        }
-        r = requests.get("https://api.qwant.com/api/search/images", params=params, headers=headers)
-        resp = r.json().get('data').get('result').get('items')[0]
-        return resp['media']
+        soup = BeautifulSoup(requests.get(url, params=params).text)
+        return soup.select('a div img')[0]['src']
 
     @commands.command(name='img')
     async def img(self, ctx, *args):
-        """Returns the first image that comes up in the search results for the given query."""
+        """Return the first image that comes up in the search results for the given query."""
         async with ctx.typing():
             query = ' '.join(args)
             emb = discord.Embed()
